44bb3342bba290939f4f7a1f790c698304f058c6
Benjamin Bork <benjamin.bork@schifferberg.com>
2024-07-22 11:59:19 +0200

    initiliazie cli

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import * as p from "@clack/prompts";
+import { setTimeout } from "node:timers/promises";
+import color from "picocolors";
+
+async function main() {
+  p.intro(
+    `${color.bgMagenta(
+      color.black(
+        " Welcome. Let us find out how much of a CLI expert you REALLY are. "
+      )
+    )}`
+  );
+
+  await setTimeout(1000);
+  console.log("Goodbye!");
+}
+
+main().catch(console.error);
